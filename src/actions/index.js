8b46156c9c2d3040153bbbdf2246ec6c2e5984a3
violateer <1828257089@qq.com>
2020-10-04 13:35:33 +0800

    显示详情页面

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,10 @@ export function fetchResource() {
     return axios.get("/api/resources").then(res => res.data)
 }
 
+export function fetchRes(resourceId) {
+    return axios.get(`/api/resources/${resourceId}`).then(res => res.data)
+}
+
 export function updateResource(id, resource) {
     return axios.patch(`/api/resources/${id}`, resource).then(res => res.data).catch(err => Promise.reject(err?.response?.data))
 }
--- src/views/ResourceDetailPage.vue
+++ src/views/ResourceDetailPage.vue
@@ -1,10 +1,41 @@
 <template>
-<h2>HELLO</h2>
+<DataDetail :resource="resource" />
 </template>
 
 <script>
+import {
+    useRoute
+} from "vue-router"
+import {
+    fetchRes
+} from "@/actions"
+import {
+    onMounted,
+    reactive,
+    toRefs
+} from "vue"
+import DataDetail from "@/components/DataDetail.vue"
 export default {
+    components: {
+        DataDetail
+    },
+    setup() {
+        const route = useRoute()
+        const data = reactive({
+            resource: null
+        })
 
+        onMounted(async () => {
+            const {
+                id
+            } = route.params
+            data.resource = await fetchRes(id)
+        })
+
+        return {
+            ...toRefs(data)
+        }
+    }
 }
 </script>
 
